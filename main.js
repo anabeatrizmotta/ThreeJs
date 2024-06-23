e8fb93925bd28f1180ed49f7956b90cfe1cdbd70
Beatriz <79261440+anabeatrizmotta@users.noreply.github.com>
2024-06-23 17:56:23 -0300

    scroll animation + light helper

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
-})
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -15,20 +15,56 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera); // render == draw
 
-
-// object
+// -- object -- 
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true});
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
+// -- light --
+
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+pointLight.position.set(4, 4, 12); // x, y, z
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
+
+// Helpers
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 150);
+scene.add(lightHelper, gridHelper)
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar)
+
+// -- background --
+// const backgroundPhoto = new THREE.TextureLoader().load( " some photo ");
+// sceme.background = backgroundPhoto;
+
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  torus.rotation.x += 0.05;
+  torus.rotation.y += 0.075;
+  torus.rotation.z += 0.05;
+
+  camera.position.x = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 function animate() {
   requestAnimationFrame(animate);
